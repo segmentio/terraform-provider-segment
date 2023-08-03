8496217e7bbb1ba5d0fda12ff0de149c770231cc
Marcelo Vargas <marcelo.pratesvargas@segment.com>
2023-08-03 10:53:38 -0700

    implemented read for warehouse metadata and tests

--- internal/provider/warehouse_metadata_data_source.go
+++ internal/provider/warehouse_metadata_data_source.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"context"
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/segmentio/public-api-sdk-go/api"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
@@ -26,6 +27,29 @@ type warehouseMetadataDataSource struct {
 	authContext context.Context
 }
 
+type Logos struct {
+	Default types.String `tfsdk:"default"`
+	Mark    types.String `tfsdk:"mark"`
+	Alt     types.String `tfsdk:"alt"`
+}
+
+type IntegrationOption struct {
+	Name        types.String `tfsdk:"name"`
+	Type        types.String `tfsdk:"type"`
+	Required    types.Bool   `tfsdk:"required"`
+	Description types.String `tfsdk:"description"`
+	Label       types.String `tfsdk:"label"`
+}
+
+type warehouseMetadataDataSourceModel struct {
+	Id          types.String        `tfsdk:"id"`
+	Name        types.String        `tfsdk:"name"`
+	Slug        types.String        `tfsdk:"slug"`
+	Description types.String        `tfsdk:"description"`
+	Logos       *Logos              `tfsdk:"logos"`
+	Options     []IntegrationOption `tfsdk:"options"`
+}
+
 func warehouseMetadataSchema() map[string]schema.Attribute {
 	return map[string]schema.Attribute{
 		"id": schema.StringAttribute{
@@ -44,25 +68,23 @@ func warehouseMetadataSchema() map[string]schema.Attribute {
 			Computed:    true,
 			Description: "A description, in English, of this object.",
 		},
-		"logos": schema.ListNestedAttribute{
+		"logos": schema.SingleNestedAttribute{
 			Computed:    true,
 			Description: "Logo information for this object.",
-			NestedObject: schema.NestedAttributeObject{
-				Attributes: map[string]schema.Attribute{
-					"default": schema.StringAttribute{
-						Computed:    true,
-						Description: "The default URL for this logo.",
-					},
-					"mark": schema.StringAttribute{
-						Optional:    true,
-						Computed:    true,
-						Description: "The logo mark.",
-					},
-					"alt": schema.StringAttribute{
-						Optional:    true,
-						Computed:    true,
-						Description: "The alternative text for this logo.",
-					},
+			Attributes: map[string]schema.Attribute{
+				"default": schema.StringAttribute{
+					Computed:    true,
+					Description: "The default URL for this logo.",
+				},
+				"mark": schema.StringAttribute{
+					Optional:    true,
+					Computed:    true,
+					Description: "The logo mark.",
+				},
+				"alt": schema.StringAttribute{
+					Optional:    true,
+					Computed:    true,
+					Description: "The alternative text for this logo.",
 				},
 			},
 		},
@@ -113,6 +135,84 @@ func (d *warehouseMetadataDataSource) Metadata(_ context.Context, req datasource
 
 // Read refreshes the Terraform state with the latest data.
 func (d *warehouseMetadataDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state warehouseMetadataDataSourceModel
+
+	diags := req.Config.Get(ctx, &state)
+
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	response, _, err := d.client.CatalogApi.GetWarehouseMetadata(d.authContext, state.Id.ValueString()).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read Warehouse metadata",
+			err.Error(),
+		)
+		return
+	}
+
+	var warehouseMetadata = response.Data.WarehouseMetadata
+
+	state.Id = types.StringValue(warehouseMetadata.Id)
+	state.Name = types.StringValue(warehouseMetadata.Name)
+	state.Description = types.StringValue(warehouseMetadata.Description)
+	state.Slug = types.StringValue(warehouseMetadata.Slug)
+	state.Logos = getLogos(warehouseMetadata.Logos)
+	state.Options = getOptions(warehouseMetadata.Options)
+
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func getOptions(options []api.IntegrationOptionBeta) []IntegrationOption {
+	var integrationOptions []IntegrationOption
+
+	for _, opt := range options {
+		integrationOption := IntegrationOption{
+			Name:     types.StringValue(opt.Name),
+			Type:     types.StringValue(opt.Type),
+			Required: types.BoolValue(opt.Required),
+		}
+
+		if opt.Description != nil {
+			integrationOption.Description = types.StringValue(*opt.Description)
+		}
+
+		if opt.Label != nil {
+			integrationOption.Label = types.StringValue(*opt.Label)
+		}
+
+		//TODO: Handle defaultValue which is a field of type Any
+		//if opt.DefaultValue != nil {
+		//	integrationOption.Label = types.value(*opt.DefaultValue)
+		//}
+
+		integrationOptions = append(integrationOptions, integrationOption)
+	}
+
+	return integrationOptions
+}
+
+func getLogos(logos api.Logos2) *Logos {
+	logosToAdd := Logos{
+		Default: types.StringValue(logos.Default),
+	}
+
+	if logos.Mark.IsSet() {
+		logosToAdd.Mark = types.StringValue(*logos.Mark.Get())
+	}
+
+	if logos.Alt.IsSet() {
+		logosToAdd.Alt = types.StringValue(*logos.Alt.Get())
+	}
+
+	return &logosToAdd
 }
 
 // Schema defines the schema for the data source.
