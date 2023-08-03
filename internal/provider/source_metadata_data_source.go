38a169018ef194a34275ef260bb4604b7be73053
Marcelo Vargas <marcelo.pratesvargas@segment.com>
2023-08-03 15:35:39 -0700

    implemented read functionality and tests for source metadata

--- internal/provider/source_metadata_data_source.go
+++ internal/provider/source_metadata_data_source.go
@@ -27,6 +27,17 @@ type sourceMetadataDataSource struct {
 	authContext context.Context
 }
 
+type sourceMetadataDataSourceModel struct {
+	Id                 types.String        `tfsdk:"id"`
+	Name               types.String        `tfsdk:"name"`
+	Slug               types.String        `tfsdk:"slug"`
+	Description        types.String        `tfsdk:"description"`
+	Logos              *Logos              `tfsdk:"logos"`
+	Options            []IntegrationOption `tfsdk:"options"`
+	Categories         []types.String      `tfsdk:"categories"`
+	IsCloudEventSource types.Bool          `tfsdk:"is_cloud_event_source"`
+}
+
 // Metadata returns the data source type name.
 func (d *sourceMetadataDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_source_metadata"
@@ -34,6 +45,67 @@ func (d *sourceMetadataDataSource) Metadata(_ context.Context, req datasource.Me
 
 // Read refreshes the Terraform state with the latest data.
 func (d *sourceMetadataDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state sourceMetadataDataSourceModel
+
+	diags := req.Config.Get(ctx, &state)
+
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	response, _, err := d.client.CatalogApi.GetSourceMetadata(d.authContext, state.Id.ValueString()).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read Source metadata",
+			err.Error(),
+		)
+		return
+	}
+
+	var sourceMetadata = response.Data.SourceMetadata
+
+	state.Id = types.StringValue(sourceMetadata.Id)
+	state.Name = types.StringValue(sourceMetadata.Name)
+	state.Description = types.StringValue(sourceMetadata.Description)
+	state.Slug = types.StringValue(sourceMetadata.Slug)
+	state.Logos = getLogosSourceMetadata(sourceMetadata.Logos)
+	state.Options = getOptions(sourceMetadata.Options)
+	state.IsCloudEventSource = types.BoolValue(sourceMetadata.IsCloudEventSource)
+	state.Categories = getCategories(sourceMetadata.Categories)
+
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func getCategories(categories []string) []types.String {
+	var categoriesToAdd []types.String
+
+	for _, cat := range categories {
+		categoriesToAdd = append(categoriesToAdd, types.StringValue(cat))
+	}
+
+	return categoriesToAdd
+}
+
+func getLogosSourceMetadata(logos api.Logos1) *Logos {
+	logosToAdd := Logos{
+		Default: types.StringValue(logos.Default),
+	}
+
+	if logos.Mark.IsSet() {
+		logosToAdd.Mark = types.StringValue(*logos.Mark.Get())
+	}
+
+	if logos.Alt.IsSet() {
+		logosToAdd.Alt = types.StringValue(*logos.Alt.Get())
+	}
+
+	return &logosToAdd
 }
 
 func sourceMetadataSchema() map[string]schema.Attribute {
@@ -54,25 +126,23 @@ func sourceMetadataSchema() map[string]schema.Attribute {
 			Computed:    true,
 			Description: "The description of this Source.",
 		},
-		"logos": schema.ListNestedAttribute{
+		"logos": schema.SingleNestedAttribute{
 			Computed:    true,
 			Description: "The logos for this Source.",
-			NestedObject: schema.NestedAttributeObject{
-				Attributes: map[string]schema.Attribute{
-					"default": schema.StringAttribute{
-						Computed:    true,
-						Description: "The default URL for this logo.",
-					},
-					"mark": schema.StringAttribute{
-						Optional:    true,
-						Computed:    true,
-						Description: "The logo mark.",
-					},
-					"alt": schema.StringAttribute{
-						Optional:    true,
-						Computed:    true,
-						Description: "The alternative text for this logo.",
-					},
+			Attributes: map[string]schema.Attribute{
+				"default": schema.StringAttribute{
+					Computed:    true,
+					Description: "The default URL for this logo.",
+				},
+				"mark": schema.StringAttribute{
+					Optional:    true,
+					Computed:    true,
+					Description: "The logo mark.",
+				},
+				"alt": schema.StringAttribute{
+					Optional:    true,
+					Computed:    true,
+					Description: "The alternative text for this logo.",
 				},
 			},
 		},
@@ -118,8 +188,7 @@ func sourceMetadataSchema() map[string]schema.Attribute {
 			Computed:    true,
 			Description: "A list of categories this Source belongs to.",
 		},
-		"is_cloud_event_source": schema.ListAttribute{
-			ElementType: types.BoolType,
+		"is_cloud_event_source": schema.BoolAttribute{
 			Computed:    true,
 			Description: "True if this is a Cloud Event Source.",
 		},
