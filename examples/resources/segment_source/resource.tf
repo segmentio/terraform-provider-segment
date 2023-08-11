56bcdef2ee2d38acb7a4cb209280008173b5f7f9
Marcelo Vargas <marcelo.pratesvargas@segment.com>
2023-08-11 10:13:53 -0700

    remove segment resource example

--- examples/resources/segment_source/resource.tf
+++
@@ -1,9 +0,0 @@
-resource "segment_source" "test" {
-  id           = "source-id"
-  slug         = "source-slug"
-  workspace_id = "w-123"
-  enabled      = true
-  metadata = {
-    id = "m-123"
-  }
-}
