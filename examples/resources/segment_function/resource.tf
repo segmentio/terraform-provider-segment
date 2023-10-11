73ca6b676c3c6d0fb0ba9594f591bdad9dfc8d1d
Dean Huynh <dean.huynh@segment.com>
2023-10-11 14:47:30 -0600

    go generate

--- examples/resources/segment_function/resource.tf
+++ examples/resources/segment_function/resource.tf
@@ -1,18 +1,18 @@
 # Configures a specific function
 resource "segment_function" "example" {
-  code = "// Learn more about source functions API at https://segment.com/docs/connections/sources/source-functions"
-  display_name = "My source function"
-  logo_url = "https://placekitten.com/200/138"
+  code          = "// Learn more about source functions API at https://segment.com/docs/connections/sources/source-functions"
+  display_name  = "My source function"
+  logo_url      = "https://placekitten.com/200/138"
   resource_type = "SOURCE"
-  description = "This is a source function."
+  description   = "This is a source function."
   settings = [
     {
-      name = "apiKey"
-      label = "api key"
-      type = "STRING"
+      name        = "apiKey"
+      label       = "api key"
+      type        = "STRING"
       description = "api key"
-      required = false
-      sensitive = false
+      required    = false
+      sensitive   = false
     },
   ]
 }
