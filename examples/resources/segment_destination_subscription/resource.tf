87d1b9b99871253fa0402414565682c8650bd4a8
Dean Huynh <dean.huynh@segment.com>
2023-10-19 13:16:05 -0700

    go generate

--- examples/resources/segment_destination_subscription/resource.tf
+++ examples/resources/segment_destination_subscription/resource.tf
@@ -1,10 +1,10 @@
 # Configures a specific destination
 resource "segment_destination_subscription" "send_to_webhook" {
   destination_id = segment_destination.webhook.id
-  name = "Webhook send subscription"
-  enabled = true
-  action_id = "abc123"
-  trigger = "type = \"track\""
+  name           = "Webhook send subscription"
+  enabled        = true
+  action_id      = "abc123"
+  trigger        = "type = \"track\""
   settings = jsonencode({
     "url" : "https://webhook.site/abc-123",
     "data" : {
