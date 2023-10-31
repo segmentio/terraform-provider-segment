4f337bcded2ba68535c846aadbc1eac1f1cf7470
Dean Huynh <dean.huynh@segment.com>
2023-10-31 13:16:06 -0700

    WIP

---
+++ internal/provider/models/insert_function_instance.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/segmentio/public-api-sdk-go/api"
+)
+
+type InsertFunctionInstanceState struct {
+	ID            types.String         `tfsdk:"id"`
+	FunctionID    types.String         `tfsdk:"function_id"`
+	IntegrationID types.String         `tfsdk:"integration_id"`
+	Name          types.String         `tfsdk:"name"`
+	Enabled       types.Bool           `tfsdk:"enabled"`
+	Settings      jsontypes.Normalized `tfsdk:"settings"`
+}
+
+func (i *InsertFunctionInstanceState) Fill(instance api.InsertFunctionInstance) error {
+	i.ID = types.StringValue(instance.Id)
+	i.FunctionID = types.StringValue(instance.ClassId)
+	i.IntegrationID = types.StringValue(instance.IntegrationId)
+	i.Name = types.StringPointerValue(instance.Name)
+	i.Enabled = types.BoolValue(instance.Enabled)
+	settings, err := GetSettingsFromMap(instance.Settings)
+	if err != nil {
+		return err
+	}
+	i.Settings = settings
+
+	return nil
+}
